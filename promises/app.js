e77e66fc2da4f584a60ac043d57c15301f9a8ec3
pavan uthsara <uthsarapavan@gmail.com>
2026-02-22 09:08:39 +0530

    promises

---
+++ promises/app.js
@@ -0,0 +1,14 @@
+const myPromise = new Promise((resolve, reject) => {
+    const condition = !true;
+    if (condition){
+        resolve('Success!');
+    } else {
+        reject('Failure!');
+    }
+});
+
+myPromise.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
